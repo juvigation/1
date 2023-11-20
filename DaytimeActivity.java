2617bf8ca769eaaa93d1f66f00f00afa683b49db
user <jwhj020401@naver.com>
2023-11-21 01:02:55 +0900

    테스트 commit

---
+++ DaytimeActivity.java
@@ -0,0 +1,94 @@
+package com.example.myapplication11;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import com.example.myapplication.R;
+
+public class DaytimeActivity extends Activity {
+
+    //Context로 다음 액티비티에서 정보 사용
+    public static Context DayContext;
+
+
+    RadioButton rdoCal, rdoTime;
+    DatePicker dPicker;
+    TimePicker tPicker;
+    TextView tvYear, tvMonth, tvDay, tvHour, tvMinute;
+    Button btnDayOk;
+    Button btnToTable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.datytime);
+        setTitle("예약 날짜 시간 선택");
+
+        //컨텍스트 설정
+        DayContext = this;
+
+
+        // FrameLayout의 2개 위젯
+        dPicker = (DatePicker) findViewById(R.id.datePicker1);
+        tPicker = (TimePicker) findViewById(R.id.timePicker1);
+
+        // 텍스트뷰 중에서 연,월,일,시,분 숫자
+        tvYear = (TextView) findViewById(R.id.tvYear);
+        tvMonth = (TextView) findViewById(R.id.tvMonth);
+        tvDay = (TextView) findViewById(R.id.tvDay);
+
+        tPicker.setVisibility(View.INVISIBLE);
+        dPicker.setVisibility(View.INVISIBLE);
+
+        rdoCal.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tPicker.setVisibility(View.INVISIBLE);
+                dPicker.setVisibility(View.VISIBLE);
+            }
+        });
+
+        rdoTime.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                tPicker.setVisibility(View.VISIBLE);
+                dPicker.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        btnDayOk = (Button)findViewById(R.id.BtnDayOk);
+        btnDayOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvYear.setText(Integer.toString(dPicker.getYear()));
+                tvMonth.setText(Integer.toString(1 + dPicker.getMonth()));
+                tvDay.setText(Integer.toString(dPicker.getDayOfMonth()));
+
+                tvHour.setText(Integer.toString(tPicker.getCurrentHour()));
+                tvMinute.setText(Integer.toString(tPicker.getCurrentMinute()));
+            }
+        });
+
+        btnToTable =  (Button)findViewById(R.id.BtnToTable);
+        btnToTable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //다음 액티비티로 가는 것
+                //Intent
+                Intent intent = new Intent(DaytimeActivity.this,TableActivity.class); //다음 Table클래스 정보 입력
+                startActivity(intent);//다음 액티비티 화면에 출력
+
+            }
+        });
+
+
+    }
+}
